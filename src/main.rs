5b06eafe842b4de00a568f1a36d50816a1d788f0
sofianeazogagh <sofiane.a.f@hotmail.fr>
2024-12-11 23:37:38 -0500

    first sketch

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod probonite;
-use probonite::probonite;
+use probonite::*;
+use revolut::{key, Context};
+use tfhe::shortint::parameters::*;
 
 fn main() {
-    let depth = 6;
-    let bit_precision = 2;
-    probonite(depth, bit_precision);
+    let mut ctx = Context::from(PARAM_MESSAGE_2_CARRY_0);
+    let private_key = key(ctx.parameters());
+    let public_key = &private_key.public_key;
+
+    let n_classes = 3;
+    let depth = 4;
+    let mut tree = Tree::generate_random_tree(depth, n_classes, &ctx);
+    tree.print_tree(&private_key, &ctx, n_classes);
+
+    let feature_vector = vec![1, 2, 3];
+    let class = 1;
+
+    let query = Query::new(&feature_vector, &class, &private_key, &mut ctx);
+    probonite(&mut tree, &query, &public_key, &ctx);
+
+    tree.print_tree(&private_key, &ctx, n_classes);
 }
--- src/probonite.rs
+++ src/probonite.rs
@@ -1,253 +1,216 @@
-// use rand::seq::index;
-use rand::{thread_rng, Rng};
-use rayon::prelude::*;
+// TFHE
+use tfhe::core_crypto::prelude::*;
 
-use tfhe::shortint::parameters::*;
-use tfhe::shortint::prelude::*;
-use tfhe::shortint::server_key::Accumulator;
+// REVOLUT
+use revolut::*;
 
-#[allow(dead_code)]
-use std::time::Instant;
+type LWE = LweCiphertext<Vec<u64>>;
 
-pub fn probonite_one_stage(
-    sks: ServerKey,
-    ct_node: (Ciphertext, Ciphertext),
-    lut: &Accumulator,
-    ct_parent_acc: Vec<Ciphertext>,
-    next_stage: Vec<(u8, u8)>,
-    cks: ClientKey,
-) -> ((Ciphertext, Ciphertext), Vec<Ciphertext>) {
-    let stage = (next_stage.len() as f64).log2();
-    println!(
-        "---------- STAGE {} to {} ----------",
-        stage as usize - 1,
-        stage
-    );
+struct Root {
+    threshold: u64,
+    feature_index: u64,
+}
 
-    let mut ct_one = sks.create_trivial(1);
+impl Root {
+    pub fn print(&self) {
+        println!("({},{})", self.threshold, self.feature_index);
+    }
+}
 
-    let ct_index_feature = ct_node.0;
-    let mut ct_theta = ct_node.1;
+pub struct InternalNode {
+    threshold: u64,
+    feature_index: u64,
+}
 
-    // BAAcc
-    let mut ct_feat = sks.keyswitch_programmable_bootstrap(&ct_index_feature, lut);
+impl InternalNode {
+    pub fn print(&self) {
+        print!("({},{})", self.threshold, self.feature_index);
+    }
+}
 
-    // CMP
-    let start_cmp = Instant::now();
-    let mut ct_cp = sks.smart_greater_or_equal(&mut ct_feat, &mut ct_theta);
-    let duration_cmp = start_cmp.elapsed();
-    println!("TIME CMP : {:?}", duration_cmp);
-    let not_ct_cp = sks.smart_sub(&mut ct_one, &mut ct_cp);
+pub struct Leaf {
+    counts: LUT,
+}
 
-    //AccAgg
-    let mut ct_childs_acc: Vec<_> = ct_parent_acc
-        .par_iter()
-        .flat_map(|ct_acc| {
-            let ct_child_left = sks.smart_mul_lsb(&mut ct_acc.clone(), &mut ct_cp.clone());
-            let ct_child_right = sks.smart_mul_lsb(&mut ct_acc.clone(), &mut not_ct_cp.clone());
+impl Leaf {
+    pub fn print(&self, private_key: &PrivateKey, ctx: &Context, n_classes: u64) {
+        // self.counts.print(private_key, ctx);
+        let array = self.counts.to_array(private_key, ctx);
+        print!("{:?}", &array[..n_classes as usize]);
+    }
+}
 
-            vec![ct_child_left, ct_child_right]
-        })
-        .collect();
+pub struct Tree {
+    root: Root,
+    nodes: Vec<Vec<InternalNode>>,
+    leaves: Vec<Leaf>,
+}
 
-    ///////// BNS
-
-    //Absorb the nodes
-
-    let mut ct_thetas: Vec<_> = ct_childs_acc
-        .par_iter_mut()
-        .enumerate()
-        .map(|(i, ct_acc)| sks.smart_scalar_mul(ct_acc, next_stage[i].1))
-        .collect();
-
-    let mut ct_indexs: Vec<_> = ct_childs_acc
-        .par_iter_mut()
-        .enumerate()
-        .map(|(i, ct_acc)| sks.smart_scalar_mul(ct_acc, next_stage[i].0))
-        .collect();
-
-    let mut ct_next_theta = ct_thetas[0].clone();
-    for i in 1..ct_thetas.len() {
-        ct_next_theta = sks.smart_add(&mut ct_next_theta, &mut ct_thetas[i]);
+impl Tree {
+    pub fn new() -> Self {
+        Self {
+            root: Root {
+                threshold: 0,
+                feature_index: 0,
+            },
+            nodes: Vec::new(),
+            leaves: Vec::new(),
+        }
     }
 
-    let mut ct_next_index = ct_indexs[0].clone();
-    for i in 1..ct_thetas.len() {
-        ct_next_index = sks.smart_add(&mut ct_next_index, &mut ct_indexs[i]);
-    }
+    pub fn generate_random_tree(depth: u64, n_classes: u64, ctx: &Context) -> Self {
+        let mut tree = Self::new();
 
-    let output_theta = cks.decrypt(&ct_next_theta);
-    let output_index = cks.decrypt(&ct_next_index);
+        // Generate the root
+        tree.root.threshold = rand::random::<u64>() % ctx.full_message_modulus() as u64;
+        tree.root.feature_index = rand::random::<u64>() % ctx.full_message_modulus() as u64;
 
-    println!("Index {} ; Theta {}", output_index, output_theta);
-    let ct_res = (ct_next_index, ct_next_theta);
-    return (ct_res, ct_childs_acc);
-}
+        // Generate the nodes
+        // Generate internal nodes for each level
+        for _ in 1..depth {
+            let mut stage = Vec::new();
 
-fn features(x: u64) -> u64 {
-    let vector: Vec<u64> = (0..32).collect();
-    let result = vector.get(x as usize);
+            // Number of nodes at this level is 2^level_index
+            let num_nodes = 2u64.pow(tree.nodes.len() as u32 + 1);
 
-    return match result {
-        Some(result) => *result,
-        None => 0,
-    };
-}
-
-#[allow(dead_code)]
-fn random_numbers_up_to_n(n: u64, num_elements: u64) -> Vec<u64> {
-    let mut rng = thread_rng();
-    (0..num_elements).map(|_| rng.gen_range(0..=n)).collect()
-}
-
-#[allow(dead_code)]
-fn cast_to_u8(v: Vec<u64>) -> Vec<u8> {
-    v.iter().map(|&x| x as u8).collect()
-}
-
-pub fn probonite_first_stage(
-    sks: ServerKey,
-    node: (u8, u8),
-    lut: &Accumulator,
-    next_stage: Vec<(u8, u8)>,
-    cks: ClientKey,
-) -> ((Ciphertext, Ciphertext), Vec<Ciphertext>) {
-    let stage = (next_stage.len() as f64).log2();
-    println!(
-        "---------- STAGE {} to {} ----------",
-        stage as usize - 1,
-        stage
-    );
-
-    let index_feat = node.0;
-    let theta = node.1;
-
-    //BAAcc
-    let trivial_index = sks.create_trivial(index_feat as u64);
-    let mut ct_feat = sks.keyswitch_programmable_bootstrap(&trivial_index, lut);
-
-    //CMP
-    let mut ct_theta = sks.create_trivial(theta as u64);
-    let mut ct_cp = sks.smart_greater(&mut ct_feat, &mut ct_theta);
-
-    //ACCAgg
-    let mut ct_one = sks.create_trivial(1);
-    let not_ct_cp = sks.smart_sub(&mut ct_one, &mut ct_cp);
-
-    let mut ct_childs_acc = vec![ct_cp, not_ct_cp];
-
-    // BNS
-    //Absorption in parallel
-    let ct_thetas: Vec<_> = ct_childs_acc
-        .par_iter_mut()
-        .enumerate()
-        .map(|(i, ct_acc)| sks.smart_scalar_mul(ct_acc, next_stage[i].1))
-        .collect();
-
-    let ct_index: Vec<_> = ct_childs_acc
-        .par_iter_mut()
-        .enumerate()
-        .map(|(i, ct_acc)| sks.smart_scalar_mul(ct_acc, next_stage[i].0))
-        .collect();
-
-    //Sum not in place
-    let mut ct_theta_0 = ct_thetas[0].clone();
-    let mut ct_index_0 = ct_index[0].clone();
-    let mut ct_theta_1 = ct_thetas[1].clone();
-    let mut ct_index_1 = ct_index[1].clone();
-    let ct_theta = sks.smart_add(&mut ct_theta_0, &mut ct_theta_1);
-    let ct_index = sks.smart_add(&mut ct_index_0, &mut ct_index_1);
-
-    let output_index = cks.decrypt(&ct_index);
-    let output_theta = cks.decrypt(&ct_theta);
-    println!("Index {} ; Theta {}", output_index, output_theta);
-
-    let ct_res = (ct_theta, ct_index);
-
-    return (ct_res, ct_childs_acc);
-}
-
-fn build_decision_tree(depth: usize, p: u8) -> Vec<Vec<(u8, u8)>> {
-    let mut rng = rand::thread_rng();
-    let mut tree = vec![vec![(rng.gen_range(0..=p), rng.gen_range(0..=p))]];
-
-    for _ in 1..depth {
-        let mut new_level = Vec::new();
-        let last_level_nodes = tree.last().unwrap();
-
-        for _ in 0..(last_level_nodes.len() * 2) {
-            new_level.push((rng.gen_range(0..=p), rng.gen_range(0..=p)));
+            for _ in 0..num_nodes {
+                stage.push(InternalNode {
+                    threshold: rand::random::<u64>() % ctx.full_message_modulus() as u64,
+                    feature_index: rand::random::<u64>() % ctx.full_message_modulus() as u64,
+                });
+            }
+            tree.nodes.push(stage);
         }
 
-        tree.push(new_level);
+        // Generate the leaves
+        let num_leaves = 2u64.pow(depth as u32);
+        for _ in 0..num_leaves {
+            let counts = LUT::from_vec_trivially(&vec![0u64; n_classes as usize], ctx);
+            tree.leaves.push(Leaf { counts });
+        }
+
+        tree
     }
 
-    tree
+    pub fn print_tree(&self, private_key: &PrivateKey, ctx: &Context, n_classes: u64) {
+        self.root.print();
+        for (i, stage) in self.nodes.iter().enumerate() {
+            for node in stage {
+                node.print();
+                print!(" ");
+            }
+            println!("");
+        }
+        for leaf in self.leaves.iter() {
+            leaf.print(private_key, ctx, n_classes);
+            print!(" ");
+        }
+        println!("");
+    }
 }
 
-#[allow(dead_code)]
-fn print_decision_tree(tree: &[Vec<(u8, u8)>]) {
-    for (level, nodes) in tree.iter().enumerate() {
-        println!("Level {}: ", level + 1);
-        for (index, &(left, right)) in nodes.iter().enumerate() {
-            println!("Node {}: Left: {}, Right: {}", index + 1, left, right);
+pub struct Query {
+    class: LWE,
+    features: LUT,
+}
+
+impl Query {
+    pub fn new(
+        feature_vector: &Vec<u64>,
+        class: &u64,
+        private_key: &PrivateKey,
+        ctx: &mut Context,
+    ) -> Self {
+        let feature_lut = LUT::from_vec(feature_vector, private_key, ctx);
+        let class_lwe = private_key.allocate_and_encrypt_lwe(class.clone(), ctx);
+        Self {
+            class: class_lwe,
+            features: feature_lut,
         }
     }
 }
 
-fn choose_parameter(p: u8) -> Parameters {
-    match p {
-        2 => PARAM_MESSAGE_2_CARRY_2,
-        3 => PARAM_MESSAGE_3_CARRY_3,
-        4 => PARAM_MESSAGE_4_CARRY_4,
-        _ => panic!("Invalid parameter"),
-    }
+pub fn next_accumulators(
+    accumulators: &Vec<LWE>,
+    selector_bit: &LWE,
+    public_key: &PublicKey,
+    ctx: &Context,
+) -> Vec<LWE> {
+    let not_selector_bit = public_key.not_lwe(selector_bit, ctx);
+    let mut nexts_accumulators = Vec::new();
+    accumulators.iter().for_each(|lwe| {
+        let accumulator_right = public_key.lwe_mul_encrypted_bit(lwe, selector_bit, ctx);
+        let accumulator_left = public_key.lwe_mul_encrypted_bit(lwe, &not_selector_bit, ctx);
+        nexts_accumulators.push(accumulator_right);
+        nexts_accumulators.push(accumulator_left);
+    });
+
+    nexts_accumulators
 }
 
-pub fn probonite(d: usize, p: u8) {
-    // We choose the parameters for the scheme:
-    let parameters = choose_parameter(p);
-    let (cks, sks) = gen_keys(parameters);
-    let lut = sks.generate_accumulator(|x| features(x));
+pub fn blind_node_selection(
+    nodes: &Vec<InternalNode>,
+    accumulators: &Vec<LWE>,
+    public_key: &PublicKey,
+    ctx: &Context,
+) -> (LWE, LWE) {
+    let mut thresholds = Vec::new();
+    nodes.iter().for_each(|node| {
+        thresholds.push(node.threshold);
+    });
+    let selected_threshold = public_key.private_selection(&thresholds, accumulators, ctx);
 
-    println!("Generated accumulator");
-    let tree = build_decision_tree(d + 1, cks.parameters.message_modulus.0 as u8 - 1);
+    let mut feature_indices = Vec::new();
+    nodes.iter().for_each(|node| {
+        feature_indices.push(node.feature_index);
+    });
+    let selected_feature_index = public_key.private_selection(&feature_indices, accumulators, ctx);
 
-    // print_decision_tree(&tree);
+    (selected_threshold, selected_feature_index)
+}
 
-    let mut ct_res: (Ciphertext, Ciphertext);
-    let mut ct_acc: Vec<Ciphertext>;
-
-    let start_probonite = Instant::now();
-
-    (ct_res, ct_acc) =
-        probonite_first_stage(sks.clone(), tree[0][0], &lut, tree[1].clone(), cks.clone());
-
-    println!("FRST STAGE DONE !");
-
-    for i in 2..d + 1 {
-        //tree.len() à la place de depth
-        (ct_res, ct_acc) = probonite_one_stage(
-            sks.clone(),
-            ct_res,
-            &lut,
-            ct_acc,
-            tree[i].clone(),
-            cks.clone(),
+pub fn blind_leaf_increment(
+    leaves: &mut Vec<Leaf>,
+    accumulators: &Vec<LWE>,
+    sample_class: &LWE,
+    public_key: &PublicKey,
+    ctx: &Context,
+) {
+    for i in 0..leaves.len() {
+        public_key.blind_array_increment(
+            &mut leaves[i].counts,
+            &sample_class,
+            &accumulators[i],
+            ctx,
         );
     }
+}
 
-    let duration_probonite = start_probonite.elapsed();
-    println!("TIME PROBONITE : {:?}", duration_probonite);
+pub fn probonite(tree: &mut Tree, query: &Query, public_key: &PublicKey, ctx: &Context) {
+    // First stage
+    let index = tree.root.feature_index;
+    let threshold = tree.root.threshold;
+    let feature = public_key.lut_extract(&query.features, index as usize, ctx);
+    let b = public_key.leq_scalar(&feature, threshold, ctx);
+    let not_b = public_key.not_lwe(&b, ctx);
+    let mut accumulators = vec![b, not_b];
 
-    let ct_index = ct_res.0;
-    let ct_theta = ct_res.1;
+    // Internal Stages
+    for i in 0..tree.nodes.len() {
+        let (threshold, feature_index) =
+            blind_node_selection(&tree.nodes[i], &accumulators, public_key, ctx);
 
-    let output_index = cks.decrypt(&ct_index);
-    let output_theta = cks.decrypt(&ct_theta);
+        let feature = public_key.blind_array_access(&feature_index, &query.features, ctx);
+        let b = public_key.blind_lt_bma_mv(&threshold, &feature, ctx);
+        accumulators = next_accumulators(&accumulators, &b, public_key, ctx);
+    }
 
-    println!(
-        "Prediction : Index {} ; Theta {}",
-        output_index, output_theta
+    // Last stage : increment the leaves and get the majority class through argmax
+    blind_leaf_increment(
+        &mut tree.leaves,
+        &accumulators,
+        &query.class,
+        public_key,
+        ctx,
     );
 }
